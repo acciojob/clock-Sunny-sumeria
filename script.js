231822a97ed477383b889ebb777c8b3e97a3010d
Sunny Sumeria <130832961+Sunny-sumeria@users.noreply.github.com>
2026-04-08 16:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let displayTimer = document.getElementById('timer');
+
+let currentDate = new Date();
+
+setInterval(function () {
+
+	currentDate = new Date();
+
+	displayTimer.innerHTML = currentDate.toLocaleString()
+	
+},1000);
